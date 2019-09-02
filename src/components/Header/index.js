e3c14bd4145e61ed82974ca8e53e8725004590bf
Johnatan <johnatanbrayan1@gmail.com>
2019-09-01 23:22:37 -0300

    Conserto

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ export default function Header(){
         <Container>
             <Top>
                 <Logo source={logo}/>
-                <Title>Joel</Title>
+                <Title>Johnatan</Title>
             </Top>
             <Icon name = "keyboard-arrow-down" size={20} color="#FFF"/>
 
